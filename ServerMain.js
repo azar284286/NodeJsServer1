77fa6a1cd4fcfde042d8a56c4792c6262e0c0336
azar284286 <alex_1995_21@ro.ru>
2016-02-25 23:55:02 +0300

    Version 0.0.0.0.1
    Сервер, начало.

---
+++ ServerMain.js
@@ -0,0 +1,25 @@
+﻿var http = require('http');
+
+var server =new http.Server();
+
+server.listen(1544,'127.0.0.1');
+
+server.on('request',function(req,res){
+
+var fs = require('fs');
+
+if(req.url=="/Bropri.ico")
+res.end(fs.readFileSync('Bropri.ico'));
+
+if(req.url=="/Style.css")
+res.end(fs.readFileSync('Data/Style.css', 'utf8'));
+
+if(req.url=="/info")
+var text = fs.readFileSync('ServInfo', 'utf8');
+
+else
+var text = fs.readFileSync('ServName', 'utf8');
+
+
+res.end(text);	
+});
